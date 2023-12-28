44fe4b8e90fc8ec6c5f2c2b5739e4d44227c02ac
OwlKKUni <wrx75690@student.wroclaw.merito.pl>
2023-12-29 00:55:22 +0100

    fixed small width collapse button

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 let toggle = document.querySelector("#header .toggle-button");
-let collapse = document.querySelectorAll("#header .collapse");
+let collapse = document.querySelectorAll("#header .collapse"); //Select both div's with "collapse" class at the same time 
 
 toggle.addEventListener('click' , function(){
     collapse.forEach(col => col.classList.toggle("collapse-toggle"));
